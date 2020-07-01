ede2e8a3c2fe0eac0c9e659e99dea4ffeade273d
Felipe Osornio <37190640+sk-dv@users.noreply.github.com>
2020-07-01 12:34:52 -0500

    parrafos formateados

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -38,19 +38,23 @@ function App() {
                     </div>
                 </div>
             </nav>
-            <div>
+            <div className="container">
+                <h1 className="title">sk dv.</h1>
                 <p>
                     No me pregunten porqué me puse SkDv, simplemente me gusta. Como una tatuaje bonito que ves y te quieres hacer, no hay un motivo
                     oculto, mera trivialidad.
-
+                </p>
+                <p>
                     Me gustan los pinos. Siempre que pienso en uno recuerdo lo fresco y tétrico que se siente estar en un bosque, sin embargo la
                     sensación
                     me parece agradable.
-
+                </p>
+                <p>
                     Decidí desarrollar esta página porque quería combinar las habilidades que he ido adquiriendo a lo largo de mi vida; es corta, aún
                     así
                     considero que de alguna manera puedo aportar algo interesante al mundo.
-
+                </p>
+                <p>
                     Me gustaría presentarme como Deiv, mi nombre no me gusta, por lo tanto no lo diré. Soy estudiante de Ingeniería en Sistema
                     Computacionales, dentro de un par de meses terminaré la carrera.
                 </p>
