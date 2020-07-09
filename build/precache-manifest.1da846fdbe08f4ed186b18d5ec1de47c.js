2c05b52f2582b8247fb0471d04b6fc9f9946ae71
sk dv <A01377154@itesm.mx>
2020-07-09 09:31:02 -0500

    Imagen agregada

---
+++ build/precache-manifest.1da846fdbe08f4ed186b18d5ec1de47c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b82d96065e580cbad54a7bfbc887adc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94b781b202abd91bb1aa",
+    "url": "/static/css/main.8fa50255.chunk.css"
+  },
+  {
+    "revision": "042cc5c41805f87d4ab2",
+    "url": "/static/js/2.648e234b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.648e234b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94b781b202abd91bb1aa",
+    "url": "/static/js/main.00027841.chunk.js"
+  },
+  {
+    "revision": "825106e405f19100b788",
+    "url": "/static/js/runtime-main.f7459489.js"
+  }
+]);
